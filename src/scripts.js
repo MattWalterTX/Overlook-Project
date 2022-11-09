aa4b87b187d03c6dd6bb32019d433d94b111a62b
Matt Walter <matt.walter.tx@outlook.com>
2022-11-09 16:06:02 -0600

    Feat: begin basic html/ css skeleton

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,52 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-// An example of how you tell webpack to use a CSS (SCSS) file
+// IMPORTS
 import './css/styles.css';
+import './images/turing-logo.png';
+import './images/overlook-staff.png';
+import './images/bar.png';
+import './images/communal-study.png';
+import './images/communal-study2.png';
+import './images/elevators.png';
+import './images/gold-room1.png';
+import './images/gold-room-entry.png';
+import './images/hall.png';
+import './images/room.png';
+import './images/suite-bathroom.png';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// GLOBAL VARIABLES
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// QUERY SELECTORS
+const homeView = document.querySelector('#home-view');
+const galleryView = document.querySelector('#gallery-view');
+const aboutView = document.querySelector('#about-view');
+const homeButton = document.querySelector('#home-button');
+const galleryButton = document.querySelector('#gallery-button');
+const aboutButton = document.querySelector('#about-button');
+
+// EVENT LISTENERS
+homeButton.addEventListener('click', showHome);
+galleryButton.addEventListener('click', showGallery);
+aboutButton.addEventListener('click', showAbout);
+
+
+// Views
+function showHome() {
+  galleryView.classList.add('hidden');
+  aboutView.classList.add('hidden');
+  homeView.classList.remove('hidden');
+};
+
+function showGallery() {
+  homeView.classList.add('hidden');
+  aboutView.classList.add('hidden');
+  galleryView.classList.remove('hidden');
+};
+
+function showAbout() {
+  homeView.classList.add('hidden');
+  galleryView.classList.add('hidden');
+  aboutView.classList.remove('hidden');
+};
